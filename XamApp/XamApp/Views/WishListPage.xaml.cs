b6d6d08f7566d157e001603e548553ed9b3e8283
kyle beavin <kylebeavin@gmail.com>
2020-11-13 13:33:52 -0500

    pulled back data from api

--- XamApp/XamApp/Views/WishListPage.xaml.cs
+++ XamApp/XamApp/Views/WishListPage.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,6 +18,40 @@ namespace XamApp.Views
         public WishListPage()
         {
             InitializeComponent();
+        }
+
+        private async Task<HttpResponseMessage> GetWeatherJson()
+        {
+            try
+            {
+
+            using (var client = new HttpClient())
+            {
+                    //client.BaseAddress = new Uri("http://localhost:50975");
+                    client.BaseAddress = new Uri("https://kbeavinapi.azurewebsites.net/");
+                    client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    HttpResponseMessage response = await client.GetAsync("api/test/");
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        ViewModels.ItemsViewModel Items;
+                        string content = await response.Content.ReadAsStringAsync();
+                        //Items = JsonConvert.DeserializeObject<List<ViewModels.ItemsViewModel>>(content);
+                    }
+                    return response;
+            }
+            }
+            catch (Exception ex)
+            {
+                return new HttpResponseMessage();
+            }
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await GetWeatherJson();
         }
     }
 }
